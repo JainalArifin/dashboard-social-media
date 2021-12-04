5a4647cd6a5ca2e2bc017d455ceb115e603fe28a
JainalArifin <zainalunpam05@gmail.com>
2021-12-04 12:27:21 +0700

    create feature minimalis path for import

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+const CracoAlias = require("craco-alias");
+const alias = require("./tools/alias").alias;
+
+module.exports = {
+  mode: "development",
+  // Adding Server
+  devServer: {
+    port: 8001,
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        // see in examples section
+        // source: "tsconfig",
+        // baseUrl: ".",
+        // tsConfigPath: "./tsconfig.path.json",
+        source: "jsconfig",
+        baseUrl: ".",
+        aliases: alias,
+      },
+    },
+  ],
+};
